7c15a9eb3a3e01ed9d2ee093ba5fb28be1d39953
June Lee <junelee@Junes-Laptop.local>
2024-03-21 15:45:43 -0500

    passes 5 tests

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,10 +1,13 @@
-export default function Banner() {
+function Banner() {
     return(
     <div>
         <header>
-            <h1>Orbit Report</h1>
-            <p>Click on the buttons to see the satellites in that orbit type</p>
+        <h1>Orbit Report</h1>
+        <p>Click on the buttons to see the satellites in that orbit type</p>
         </header>
+
+
     </div>
     );
 }
+export default Banner
